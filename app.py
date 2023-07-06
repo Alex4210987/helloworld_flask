6c6a1da065bd9c4d899fe15250e81b06fe326c63
Alex4210987 <u202211967@hust.edu.cn>
2023-07-06 12:49:04 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -91,3 +91,8 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    user = User.query.first()
+    return render_template('404.html', user=user), 404  # 返回模板和状态码
